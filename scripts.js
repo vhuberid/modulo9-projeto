0efe39ddca52124fa7b67b4051ac83628820b1aa
vhuberid <vanessa.idalencio@hotmail.com>
2022-12-28 20:05:20 +0100

    conteudo aulas

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 const dolar = 5.27
 const euro = 5.60
+const bitcoin = 87.29
 
 const convertValues = () => {
     const inputReais = document.getElementById("input-Real").value
@@ -24,7 +25,13 @@ const convertValues = () => {
         style: "currency",
         currency: "EUR",
     }).format(inputReais/euro)
-}
+    }
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML= new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputReais/bitcoin)
+    }
 }
 
 changeCurrency = () => {
@@ -37,6 +44,10 @@ changeCurrency = () => {
     if (select.value === "US§ Dólar Americano"){
         currencyName.innerHTML = "Dólar Americano"
         currencyImg.src = "./images/eua.svg"
+    }
+    if (select.value === "Bitcoin"){
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./images/bitcoin.png"
     }
     convertValues()
 }
